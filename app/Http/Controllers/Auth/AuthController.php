45d604a780e283a34f582f1932e2d5a4e80ed301
tsunari <mtst.tty556@gmail.com>
2021-05-05 22:49:45 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
@@ -19,15 +20,40 @@ class AuthController extends Controller
     {
         $creadentials = $request->only('email', 'password');
 
-        if (Auth::attempt($creadentials)) {
-            $request->session()->regenerate();
+        $user = User::where('email', '=', $creadentials['email'])->first();
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        if(!is_null($user)) {
+            if ($user->locked_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            if (Auth::attempt($creadentials)) {
+                $request->session()->regenerate();
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+            
+            $user->error_count = $user->error_count + 1;
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。',
+                ]);    
+            }
+            $user->save();
         }
 
-        return back()->withErrors([
-            'danger' => 'メールアドレスかパスワードが間違っています。',
-        ]);
+            return back()->withErrors([
+                'danger' => 'メールアドレスかパスワードが間違っています。',
+            ]);
     }
 
     public function logout(Request $request)
